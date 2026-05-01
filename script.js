6a6142d05066fb394962e98a0a1822ba27c30a76
Devutaraka <tarakadevu1@gmail.com>
2026-05-01 19:06:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+function addTask() {
 
+    let input = document.getElementById("taskInput");
+    let task = input.value;
+
+    let li = document.createElement("li");
+    li.textContent = task;
+
+    li.onclick = function() {
+        this.remove();
+    };
+
+    document.getElementById("taskList").appendChild(li);
+
+    input.value = "";
+}
